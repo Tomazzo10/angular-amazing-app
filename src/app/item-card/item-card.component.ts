b08c969300bdfdce87ce3ea565efc05fd3baaa38
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-06-20 15:23:44 +0200

    fixed small details

--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -1,12 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, startWith } from 'rxjs';
-import { Item } from '../model/item';
-import { ItemsService } from '../services/items.service';
+import { Component, Input, OnInit } from "@angular/core";
+import { BehaviorSubject, Observable, startWith } from "rxjs";
+import { Item } from "../model/item";
+import { ItemsService } from "../services/items.service";
 
 @Component({
-  selector: 'app-item-card',
-  templateUrl: './item-card.component.html',
-  styleUrls: ['./item-card.component.scss'],
+  selector: "app-item-card",
+  templateUrl: "./item-card.component.html",
+  styleUrls: ["./item-card.component.scss"],
 })
 export class ItemCardComponent implements OnInit {
   private titleSubject: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>(
