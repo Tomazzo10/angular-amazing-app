0d2b65a322f14b81005bb24071e9b75e02ae24fc
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-06-20 12:17:52 +0200

    added items fetch to item card

--- amazing-app/src/app/item-card/item-card.component.ts
+++ amazing-app/src/app/item-card/item-card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable, startWith } from 'rxjs';
+import { Item } from '../model/item';
+import { ItemsService } from '../services/items.service';
 
 @Component({
   selector: 'app-item-card',
   templateUrl: './item-card.component.html',
-  styleUrls: ['./item-card.component.scss']
+  styleUrls: ['./item-card.component.scss'],
 })
 export class ItemCardComponent implements OnInit {
+  @Input()
+  items$: Observable<Item[]> = [];
 
-  constructor() { }
+  constructor(private itemsService: ItemsService) {}
 
   ngOnInit(): void {
+    this.loadItems();
   }
 
+  loadItems() {
+    this.items$ = this.itemsService.loadAllItems().pipe(startWith([]));
+  }
 }
