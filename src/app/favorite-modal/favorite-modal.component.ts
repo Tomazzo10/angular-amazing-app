a920a4e041aa5e422159ca6642cf228f29ed42f1
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-06-21 11:39:09 +0200

    created and developed initial features of the modal

--- src/app/favorite-modal/favorite-modal.component.ts
+++ src/app/favorite-modal/favorite-modal.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-favorite-modal',
-  templateUrl: './favorite-modal.component.html',
-  styleUrls: ['./favorite-modal.component.scss']
+  selector: "app-favorite-modal",
+  templateUrl: "./favorite-modal.component.html",
+  styleUrls: ["./favorite-modal.component.scss"],
 })
 export class FavoriteModalComponent implements OnInit {
+  closeResult: string = "";
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  open() {
+    this.modalService.open({ ariaLabelledBy: "modal-basic-title" }).result.then(
+      res => {
+        this.closeResult = `Closed with: ${res}`;
+      },
+      reason => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
   }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
