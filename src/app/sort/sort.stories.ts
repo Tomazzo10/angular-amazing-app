44bf471f56d5efb0d99ba1b8b2b76dea05956d38
Tomazzo Antolioni <tomazzoantolioni@MBPdeTontolioni.home>
2022-06-30 16:27:54 +0200

    sort: created sort story to develop it

---
+++ src/app/sort/sort.stories.ts
@@ -0,0 +1,22 @@
+import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
+import { Meta, moduleMetadata, Story } from "@storybook/angular";
+
+import { SortComponent } from "./sort.component";
+
+export default {
+  title: "Sort",
+  component: SortComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [SortComponent],
+      imports: [NgbDropdownModule],
+    }),
+  ],
+} as Meta;
+
+export const Sort: Story = () => ({
+  props: {
+    label: "Sort",
+    primary: true,
+  },
+});
