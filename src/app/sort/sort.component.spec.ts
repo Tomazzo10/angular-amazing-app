e0a8543ba60aeb8f18e3500300e4381579360885
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-07-05 18:03:39 +0200

    Sort component: changes on linting

--- src/app/sort/sort.component.spec.ts
+++ src/app/sort/sort.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { SortComponent } from './sort.component';
+import { SortComponent } from "./sort.component";
 
-describe('SortComponent', () => {
+describe("SortComponent", () => {
   let component: SortComponent;
   let fixture: ComponentFixture<SortComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SortComponent ]
-    })
-    .compileComponents();
+      declarations: [SortComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SortComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
