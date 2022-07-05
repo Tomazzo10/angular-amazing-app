88b1261b29e340de1d0bcac0f4dd2dbd148e7e9a
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-07-05 12:29:59 +0200

    App module: removing unnecessary declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,12 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemCardComponent } from "./item-card/item-card.component";
-import { ItemsListComponent } from "./items-list/items-list.component";
-import { SearchBarComponent } from "./search-bar/search-bar.component";
 import { ReactiveFormsModule } from "@angular/forms";
-import { FavoriteButtonComponent } from "./favorite-button/favorite-button.component";
-import { FavoriteModalComponent } from "./favorite-modal/favorite-modal.component";
-import { FavoriteItemListComponent } from "./favorite-item-list/favorite-item-list.component";
 
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ItemCardComponent,
-    ItemsListComponent,
-    FavoriteButtonComponent,
-    FavoriteModalComponent,
-    FavoriteItemListComponent,
-    SearchBarComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
