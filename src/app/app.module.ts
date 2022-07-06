128dca7e77ed4c00224158efcf68feda75a17605
Tomazzo Antolioni <tomazzoantolioni@MBPdeTontolioni.home>
2022-07-06 17:53:00 +0200

    App module: updating declarations and imports to support the price filter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,23 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ReactiveFormsModule } from "@angular/forms";
+import { PriceFilterComponent } from "./price-filter/price-filter.component";
 
 import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PriceFilterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
     NgbDropdownModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
