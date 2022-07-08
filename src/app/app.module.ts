b72110963ca3b06b9a7cce04ab3b8e0f445edbbe
Tomazzo Antolioni <tomazzoantolioni@MBPdeTontolioni.home>
2022-07-08 13:54:57 +0200

    mergin from main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,15 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { PriceFilterComponent } from "./price-filter/price-filter.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [AppComponent, PriceFilterComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
