7453ff7c7507785646ff39dfad1af86858a87628
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-07-11 11:04:33 +0100

    App module: added component to the dependencies of the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ReactiveFormsModule } from "@angular/forms";
+import { PriceFilterComponent } from "./price-filter/price-filter.component";
 
 import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PriceFilterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
