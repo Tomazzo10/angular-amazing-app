b491092d41494671e9989fd85e416927be0f64c9
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-07-12 12:20:21 +0100

    App module: refactored file, removing unnecessary components and moving them to their own module, with the respective necessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,10 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ReactiveFormsModule } from "@angular/forms";
-import { SearchBarComponent } from "./search-bar/search-bar.component";
-import { SelectorComponent } from "./selector/selector.component";
-
-import { NgbDropdownModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [AppComponent, SearchBarComponent, SelectorComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    NgbDropdownModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
