a6d7d30dfc6da9429eca14b8e14a5cb52da7dcf8
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-07-13 10:29:59 +0100

    Slider: small fixes on the component

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -3,6 +3,7 @@ import {
   Input,
   Output,
   ChangeDetectionStrategy,
+  EventEmitter,
 } from "@angular/core";
 
 import { SliderConfig } from "../model/sliderConfig.interface";
@@ -16,22 +17,22 @@ import { SliderConfig } from "../model/sliderConfig.interface";
 export class SliderComponent {
   public min!: number;
   public max!: number;
-  public valueSelected!: number;
+  public value!: number;
   public unit!: string;
   public description!: string;
 
   @Input() set config(config: SliderConfig) {
     this.min = config.min;
     this.max = config.max;
-    this.valueSelected = config.max;
+    this.value = config.max;
     this.unit = config.unit;
     this.description = config.description;
   }
 
   @Output()
-  selectedMaxValue!: number;
+  selectedMaxValue = new EventEmitter<number>();
 
   public selectMaxValue(value: number): void {
-    this.selectedMaxValue = value;
+    this.selectedMaxValue.emit(value);
   }
 }
