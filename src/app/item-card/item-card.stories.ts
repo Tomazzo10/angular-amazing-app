8161b867d78952860e75dd8ff7d68bd47d780a82
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-07-14 15:00:49 +0100

    Item card story: created a new instance for long texts

--- src/app/item-card/item-card.stories.ts
+++ src/app/item-card/item-card.stories.ts
@@ -20,7 +20,7 @@ export const ItemCardNotFavorited: Story = () => ({
       id: "j2vg1n31g313g",
       title: "Wallapop",
       description:
-        "This is a very long description of an item to test how long descriptions are going to work in the application",
+        "This is a long description of an item to test how long descriptions are going to work in the application",
       image:
         "https://freepikpsd.com/file/2019/10/wallapop-logo-png-3-Transparent-Images-Free.png",
       price: {
@@ -38,7 +38,7 @@ export const ItemCardStoryFavorited: Story = () => ({
       id: "j2vg1n31g313g",
       title: "Wallapop",
       description:
-        "This is a very long description of an item to test how long descriptions are going to work in the application",
+        "This is a long description of an item to test how long descriptions are going to work in the application",
       image:
         "https://freepikpsd.com/file/2019/10/wallapop-logo-png-3-Transparent-Images-Free.png",
       price: {
@@ -46,6 +46,24 @@ export const ItemCardStoryFavorited: Story = () => ({
         currency: "EUR",
       },
       favorited: true,
+    },
+  },
+});
+
+export const ItemCardLongString: Story = () => ({
+  props: {
+    item: {
+      id: "j2vg1n31g313g",
+      title: "Wallapop pop pop pop pop",
+      description:
+        "This is a very long long long long long long long long long long description of an item to test how long descriptions are going to work in the application",
+      image:
+        "https://freepikpsd.com/file/2019/10/wallapop-logo-png-3-Transparent-Images-Free.png",
+      price: {
+        amount: 100,
+        currency: "EUR",
+      },
+      favorited: false,
     },
   },
 });
