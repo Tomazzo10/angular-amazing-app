415b44f0989e4a0b6731d89edadccfc248f2f531
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-07-25 13:00:51 -0300

    Home page story: added imports needed, initial args for the components and created 2 stories for a list with items and another with no items

--- src/app/home-page/home-page.stories.ts
+++ src/app/home-page/home-page.stories.ts
@@ -1,19 +1,183 @@
 import { Meta, Story } from "@storybook/angular";
 import { moduleMetadata } from "@storybook/angular";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
 
 import { HomePageComponent } from "./home-page.component";
 import { HeaderModule } from "./header/header.module";
 import { ItemsListModule } from "./items/items-list/items-list.module";
 
 export default {
-  title: "Combining/Header",
+  title: "AssemblingPages/HomePage",
   component: HomePageComponent,
+  argTypes: {
+    items: [],
+  },
   decorators: [
     moduleMetadata({
       declarations: [HomePageComponent],
-      imports: [HeaderModule, ItemsListModule],
+      imports: [CommonModule, BrowserModule, HeaderModule, ItemsListModule],
     }),
   ],
 } as Meta;
 
-export const HeaderStory: Story = () => ({});
+export const HomePageStoryNoItems: Story = () => ({});
+
+export const HomePageStoryWithItems: Story = () => ({
+  props: {
+    items: [
+      {
+        id: "j2vg1n31g313g",
+        title: "Wallapop pop pop pop pop",
+        description:
+          "This is a long description of an item to test how long descriptions are going to work in the application",
+        image:
+          "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+        price: {
+          amount: 100,
+          currency: "BRL",
+        },
+        favorited: false,
+      },
+      {
+        id: "j2vg1n31g313g",
+        title: "Wallapop",
+        description: "This is a description",
+        image:
+          "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+        price: {
+          amount: 1000,
+          currency: "EUR",
+        },
+        favorited: false,
+      },
+      {
+        id: "j2vg1n31g313g",
+        title: "Wallapop",
+        description:
+          "This is a long long long long long long long long long long long long description of an item to test how long descriptions are going to work in the application",
+        image:
+          "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+        price: {
+          amount: 1,
+          currency: "GBP",
+        },
+        favorited: true,
+      },
+      {
+        id: "j2vg1n31g313g",
+        title: "Wallapop",
+        description:
+          "This is a long description of an item to test how long descriptions are going to work in the application",
+        image:
+          "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+        price: {
+          amount: 100,
+          currency: "EUR",
+        },
+        favorited: false,
+      },
+      {
+        id: "j2vg1n31g313g",
+        title: "Wallapop pop pop pop pop",
+        description:
+          "This is a long description of an item to test how long descriptions are going to work in the application",
+        image:
+          "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+        price: {
+          amount: 10,
+          currency: "EUR",
+        },
+        favorited: false,
+      },
+      {
+        id: "j2vg1n31g313g",
+        title: "Wallapop",
+        description: "This is a description",
+        image:
+          "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+        price: {
+          amount: 100000,
+          currency: "EUR",
+        },
+        favorited: false,
+      },
+      {
+        id: "j2vg1n31g313g",
+        title: "Wallapop pop pop pop",
+        description: "This is a description",
+        image:
+          "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+        price: {
+          amount: 1,
+          currency: "GBP",
+        },
+        favorited: false,
+      },
+      {
+        id: "j2vg1n31g313g",
+        title: "Wallapop",
+        description:
+          "This is a long description of an item to test how long descriptions are going to work in the application",
+        image:
+          "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+        price: {
+          amount: 10,
+          currency: "EUR",
+        },
+        favorited: true,
+      },
+      {
+        id: "j2vg1n31g313g",
+        title: "Wallapop",
+        description: "This is a description",
+        image:
+          "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+        price: {
+          amount: 10000,
+          currency: "BRL",
+        },
+        favorited: false,
+      },
+      {
+        id: "j2vg1n31g313g",
+        title: "Wallapop",
+        description:
+          "This is a long description of an item to test how long descriptions are going to work in the application",
+        image:
+          "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+        price: {
+          amount: 100,
+          currency: "EUR",
+        },
+        favorited: false,
+      },
+      {
+        id: "j2vg1n31g313g",
+        title: "Wallapop",
+        description:
+          "This is a long description of an item to test how long descriptions are going to work in the application",
+        image:
+          "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+        price: {
+          amount: 100,
+          currency: "GBP",
+        },
+        favorited: false,
+      },
+      {
+        id: "j2vg1n31g313g",
+        title: "Wallapop pop pop pop",
+        description:
+          "This is a long long long long long long long long long long long long description of an item to test how long descriptions are going to work in the application",
+        image:
+          "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+        price: {
+          amount: 1000,
+          currency: "USD",
+        },
+        favorited: true,
+      },
+    ],
+  },
+});
