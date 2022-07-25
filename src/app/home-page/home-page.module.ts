933406d259646bb1598432e98c49ef6358466778
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-07-25 13:03:20 -0300

    Home page module: removed from the module unnecessary import

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
 
 import { HomePageComponent } from "./home-page.component";
 import { HeaderModule } from "./header/header.module";
 import { ItemsListModule } from "./items/items-list/items-list.module";
 
 @NgModule({
-  imports: [CommonModule, HeaderModule, ItemsListModule],
+  imports: [HeaderModule, ItemsListModule],
   exports: [HomePageComponent],
   declarations: [HomePageComponent],
   providers: [],
