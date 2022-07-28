50fb3659d4add80998be07342b0219f8a843dd35
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-07-28 13:24:05 -0300

    Item detail: created and built module of the component

---
+++ src/app/item-detail/item-detail.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+
+import { ItemDetailComponent } from "./item-detail.component";
+
+@NgModule({
+  imports: [],
+  declarations: [ItemDetailComponent],
+  exports: [ItemDetailComponent],
+})
+export class ItemsDetailModule {}
