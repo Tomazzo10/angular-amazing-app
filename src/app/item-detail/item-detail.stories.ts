add32b35953d3c67acd6257a47eede0489e046e1
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-07-28 13:27:59 -0300

    Item detail story: created story for the item detail to develop the component

---
+++ src/app/item-detail/item-detail.stories.ts
@@ -0,0 +1,33 @@
+import { Meta, Story } from "@storybook/angular";
+import { moduleMetadata } from "@storybook/angular";
+
+import { ItemDetailComponent } from "./item-detail.component";
+
+export default {
+  title: "Component/Items-Detail",
+  component: ItemDetailComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [ItemDetailComponent],
+      imports: [],
+    }),
+  ],
+} as Meta;
+
+export const ItemsDetailStory: Story = () => ({
+  props: {
+    item: {
+      id: "j2vg1n31g313g",
+      title: "Wallapop pop pop pop pop",
+      description:
+        "This is a long description of an item to test how long descriptions are going to work in the application",
+      image:
+        "https://play-lh.googleusercontent.com/zbQSHTc9DGlNkV8sqyVQCiE7H700fJi3dwfcKypbLcBz81N15RiL7RacGmFM70uVhaO6",
+      price: {
+        amount: 100,
+        currency: "BRL",
+      },
+      favorited: false,
+    },
+  },
+});
