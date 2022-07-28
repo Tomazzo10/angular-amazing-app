df870a7f86c1dc5e305782d2c230060b8d1a3995
Tomazzo Antolioni <tomazzoantolioni@MacBook-Pro-de-Tomazzo-Antolioni.local>
2022-07-28 06:13:16 -0300

    Search Bar: removed method onChange because the form is being subscribed now to listen for changed in the input

--- src/app/header/components/search-bar/search-bar.component.ts
+++ src/app/header/components/search-bar/search-bar.component.ts
@@ -18,11 +18,20 @@ export class SearchBarComponent {
   public searchForm = new FormGroup<SearchFormGroup>({
     search: new FormControl("", { nonNullable: true }),
   });
-  public searchBoxValue: string | undefined = "";
   public resetIconFlag: boolean = false;
 
   @Output()
   searchValue = new EventEmitter<string>();
+
+  constructor() {
+    this.searchBarChangeDetection();
+  }
+
+  private searchBarChangeDetection(): void {
+    this.searchForm.get("search")?.valueChanges.subscribe(selectedValue => {
+      this.resetIconFlag = !!selectedValue.length;
+    });
+  }
 
   public onSubmit(): void {
     this.searchValue.emit(this.searchForm.get("search")?.value);
@@ -31,12 +40,5 @@ export class SearchBarComponent {
   public resetForm(): void {
     this.searchForm.reset();
     this.resetIconFlag = false;
-  }
-
-  public onChange(): void {
-    this.searchBoxValue = this.searchForm.get("search")?.value;
-    this.searchBoxValue?.length
-      ? (this.resetIconFlag = true)
-      : (this.resetIconFlag = false);
   }
 }
